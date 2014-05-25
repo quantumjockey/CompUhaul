527694a9f2f2973dc81bc00d6f2f9809beec4aeb
quantumjockey <nickyd242@gmail.com>
2014-05-24 17:39:42 -0700

    Added path validation object.

---
+++ CompUhaul.Test/Paths/path_spec.cs
@@ -0,0 +1,87 @@
+﻿///////////////////////////////////////
+#region Namespace Directives
+
+using CompUhaul.Paths;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+#endregion
+///////////////////////////////////////
+
+namespace CompUhaul.Test.Paths
+{
+    [TestClass]
+    public class path_spec
+    {
+        ////////////////////////////////////////
+        #region Constants
+
+        const string testDirectory = @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul\Paths";
+        const string testFile = @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul\Paths\path.cs";
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Unit Tests (Methods)
+
+        // Constructor
+
+        [TestMethod]
+        public void Constructor_PathInvalid_ExistsIsFalse()
+        {
+            path stuff = new path(null);
+            Assert.IsFalse(stuff.Exists);
+        }
+
+        [TestMethod]
+        public void Constructor_ValidDirectory_ExistsIsTrue()
+        {
+            path stuff = new path(testDirectory);
+            Assert.IsTrue(stuff.Exists);
+        }
+
+        [TestMethod]
+        public void Constructor_ValidFile_ExistsIsTrue()
+        {
+            path stuff = new path(testFile);
+            Assert.IsTrue(stuff.Exists);
+        }
+
+        // GetHeirarchy
+
+        [TestMethod]
+        public void GetHeirarchy_ValidDirectory_PathParsed()
+        {
+            string[] parsed = new string[]{
+                @"D:",
+                @"D:\Visual C# Application Source",
+                @"D:\Visual C# Application Source\Libraries",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul\Paths"
+            };
+
+            path stuff = new path(testDirectory);
+            CollectionAssert.AreEqual(parsed, stuff.GetHeirarchy());
+        }
+
+        [TestMethod]
+        public void GetHeirarchy_ValidFile_PathParsed()
+        {
+            string[] parsed = new string[]{
+                @"D:",
+                @"D:\Visual C# Application Source",
+                @"D:\Visual C# Application Source\Libraries",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul\Paths",
+                @"D:\Visual C# Application Source\Libraries\CompUhaul\CompUhaul\Paths\path.cs"
+            };
+
+            path stuff = new path(testFile);
+            CollectionAssert.AreEqual(parsed, stuff.GetHeirarchy());
+        }
+
+        #endregion
+    }
+}
---
+++ CompUhaul/Paths/path.cs
@@ -0,0 +1,85 @@
+﻿///////////////////////////////////////
+#region Namespace Directives
+
+using System.Collections.Generic;
+using System.IO;
+
+#endregion
+///////////////////////////////////////
+
+namespace CompUhaul.Paths
+{
+    public class path
+    {
+        ////////////////////////////////////////
+        #region Generic Fields
+
+        string _fullPath;
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Properties
+
+        public bool Exists
+        {
+            get { return CheckIfPathExists(_fullPath); }
+        }
+
+        public string FullPath
+        {
+            get { return _fullPath; }
+            private set { _fullPath = value; }
+        }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Constructor
+
+        public path(string specifiedPath)
+        {
+            FullPath = specifiedPath;
+        }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Public Methods
+
+        /// <summary>
+        /// Returns the complete heiarchy from the file or directory specified down to the drive path.
+        /// </summary>
+        /// <returns>Array containing sequential paths leading to the specified path.</returns>
+        public string[] GetHeirarchy()
+        {
+            char pathSeparator = '\\';
+            List<string> heirarchy = new List<string>();
+
+            string[] partials = FullPath.Split(pathSeparator);
+
+            for (int i = 0; i < partials.Length; i++)
+                if (i > 0)
+                    heirarchy.Add(heirarchy[i - 1] + pathSeparator + partials[i]);
+                else
+                    heirarchy.Add(partials[i]);
+
+            return heirarchy.ToArray();
+        }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Supporting Methods
+
+        private bool CheckIfPathExists(string path)
+        {
+            if (File.Exists(path) || Directory.Exists(path))
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+    }
+}
